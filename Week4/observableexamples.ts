7995562345283436a507d011ed9f011a8985db8c
Haider Ali <hali0010@student.monash.edu>
2021-08-28 15:41:03 +1000

    added observabes example to week 4

--- Week4/observableexamples.ts
+++ Week4/observableexamples.ts
@@ -11,8 +11,8 @@ Person 4... |           |       | 25%           |
 complete Worksheet 4/5 by entering code in the places marked below...
 */
 
-import { interval, fromEvent } from 'rxjs'
-import { map, filter} from 'rxjs/operators'
+import { interval, fromEvent, zip, range,Observable} from 'rxjs'
+import { map, filter,flatMap,takeUntil,scan,take,merge} from 'rxjs/operators'
 
 // Simple demonstration 
 // ===========================================================================================
@@ -73,7 +73,7 @@ function piApproximation() {
     a = 1103515245
     c = 12345
     state:number
-    constructor(seed) {
+    constructor(seed:number) {
       this.state = seed ? seed : Math.floor(Math.random() * (this.m - 1));
     }
     nextInt() {
@@ -104,6 +104,7 @@ function piApproximation() {
     totalCount: number
   }
 
+  const data = {insideCount: 0, totalCount:0}
   // an instance of the Random Number Generator with a specific seed
   const rng = new RNG(20)
   // return a random number in the range [-1,1]
@@ -117,24 +118,42 @@ function piApproximation() {
 
   // Your code starts here!
   // =========================================================================================
-  function createDot(/* what parameters do we need to plot dots at different locations and in red or green? Use the types above! */) {
+  function createDot(xIn:number, yIn:number) {
     if (!canvas) throw "Couldn't get canvas element!";
     const dot = document.createElementNS(canvas.namespaceURI, "circle");
-    const x = 50, y = 50; // all points are at 50,50!
-    // Set circle properties
-    dot.setAttribute("cx", String(x));
-    dot.setAttribute("cy", String(y));
-    dot.setAttribute("r", "5");
-    dot.setAttribute("fill", "red");// All points red
-
+    let newDot = {x: xIn+circleRadius,y: yIn+circleRadius, colour:"red"}
+    inCircle({x:xIn,y:yIn})? newDot = {x:xIn*circleRadius,y:yIn*circleRadius,colour:"green"}: newDot = {x:xIn*circleRadius,y:yIn*circleRadius,colour:"red"}
+    if (newDot.colour == "green") {data.insideCount++}
+    data.totalCount++
+    resultInPage.innerText = (4*data.insideCount/data.totalCount).toString()
+    //Set circle properties
+    dot.setAttribute("cx", String(xIn*circleRadius+circleRadius));
+    dot.setAttribute("cy", String(yIn*circleRadius+circleRadius));
+    dot.setAttribute("r", '5');
+    dot.setAttribute("fill", newDot.colour);
+  
     // Add the dot to the canvas
     canvas.appendChild(dot);
-  }
 
+    
+  }
+  // method in rxjs zipWith 
   // A stream of random numbers
   const randomNumberStream = interval(50).pipe(map(nextRandom))
+  const randPairs = zip(randomNumberStream, randomNumberStream).subscribe(pair => createDot(pair[0], pair[1]))
+  
 
+  
+// zip(randomNumberStream, randomNumberStream.pipe((
+//     map(() => ([Rand1, Rand2])))
+//       .subscribe(x => createDot(Rand1, Rand2));
+
+  //const randomNumberStream = interval(50).pipe(map(zip)).subscribe((, nextRandom) =createDot)
+  
 }
+
+// o.pipe(map(({ x, y }) => `${x},${y}`))
+// .subscribe((s : string) => pos.innerHTML = s);
 
 // Exercise 6
 // ===========================================================================================
@@ -171,8 +190,40 @@ function animatedRect() {
   // Your code starts here!
   // =========================================================================================
   // ...
+  const svg = document.getElementById("animatedRect")!;
+  // create the rect
+  const rect = document.createElementNS(svg.namespaceURI,'rect')
+  Object.entries({
+    x: 100, y: 70,
+    width: 120, height: 80,
+    fill: '#95B3D7',
+  }).forEach(([key,val])=>rect.setAttribute(key,String(val)))
+  svg.appendChild(rect);
 
+
+  const oneStream = interval(50).pipe(map(x=>5))
+  const stream1 = oneStream.subscribe(moveRectangle)
+  // const timer = Observable<number>  = timer(1000)
+  // const timerStream = timer.subscribe(checkTime)
+
+ setTimeout(() => {
+  stream1.unsubscribe();
+}, 1000);
+
+
+  function moveRectangle(amount:number) {
+    rect.setAttribute('x', String(amount + Number(rect.getAttribute('x'))));
+  }
+
+  // function checkTime(time:number) {
+  //   if (time > 1000) {
+  //     stream1.unsubscribe()
+  //     timerStream.unsubscribe()
+  //   }
+  // }
 }
+
+
 
 // Exercise 7
 // ===========================================================================================
@@ -181,14 +232,82 @@ function animatedRect() {
  * Create and control a rectangle using the keyboard! Use only one subscribe call and not the interval method
  * If statements  
  */
+
+type Char ='x' |'y'
 function keyboardControl() {
   // get the svg canvas element
   const svg = document.getElementById("moveableRect")!;
-  
+  const rect = document.createElementNS(svg.namespaceURI,'rect')
+  Object.entries({
+    x: 100, y: 70,
+    width: 120, height: 80,
+    fill: '#95B3D7',
+  }).forEach(([key,val])=>rect.setAttribute(key,String(val)))
+  svg.appendChild(rect);
+
+
+  const MOVEMENT_SPEED = 4; // One button press moves the rectangle this many coordinates
+  const arrowDown$ = fromEvent<KeyboardEvent>(document, 'keydown').pipe(filter(event => event.key == 'ArrowDown'), map(_ => ({x: 0, y: 1})));
+  const arrowUp$ = fromEvent<KeyboardEvent>(document, 'keydown').pipe(filter(event => event.key == 'ArrowUp'), map(_ => ({x: 0, y: -1})), merge(arrowDown$));
+  const arrowLeft$ = fromEvent<KeyboardEvent>(document, 'keydown').pipe(filter(event => event.key == 'ArrowLeft'), map(_ => ({x: -1, y: 0})), merge(arrowUp$));
+  const arrowRight$ = fromEvent<KeyboardEvent>(document, 'keydown').pipe(filter(event => event.key == 'ArrowRight'), map(_ => ({x: 1, y: 0})), merge(arrowLeft$))
+  .subscribe(({x, y}) => { 
+    rect.setAttribute('x', String(x*MOVEMENT_SPEED + Number(rect.getAttribute('x')))); 
+    rect.setAttribute('y', String(y*MOVEMENT_SPEED + Number(rect.getAttribute('y'))));
+  });
+  /*
+function moveRectangle2(axis:Char,amount:number) {
+  rect.setAttribute(axis, String(Number(rect.getAttribute(axis))+amount))
+}
+
+
+  fromEvent<KeyboardEvent>(document, 'keydown')
+  .pipe(
+    //Filters out all events except for the ones we want. Left/Right for moving, Up arrow for shooting
+    filter(({code})=>code === 'ArrowLeft' || code === 'ArrowRight' || code === 'ArrowUp' || code === 'ArrowDown'),
+    //remove repeat keys
+    filter(({repeat})=>!repeat),
+    //Has 8 millisecond interval between movements, to avoid hyper movements
+    flatMap(d=>interval(8).pipe(
+      //Stops filtering once key is released, as backlog of keydown events would occur
+      takeUntil(fromEvent<KeyboardEvent>(document, 'keyup').pipe(
+        //returns code object as code
+        filter(({code})=>code === d.code)
+      )),
+      map(_=>d))
+    ),
+    //Moves xaxis by 1 or -1 depending on which key is pressed.
+    // map(({code})=>code==='ArrowLeft'? ['x',-1]:'ArrowRight'? ['x',1] : 'ArrowUp'? ['y',1] : ['y',-1]).scan(moveRectangle2, rect)
+
+    map(({code})=>    
+    code==='ArrowLeft'? moveRectangle2('x',(-1)) :
+    (code ==='ArrowRight')? moveRectangle2('x',1):
+    (code ==='ArrowUp')? moveRectangle2('y',(-1)) : rect.setAttribute('y', String(Number(rect.getAttribute('y'))+1))), 
+
+
+
+    scan(moveRectangle2, rect)
+    
+    
+    
+    // .subscribe(moveRectangle2)
+
+*/
+}
+
+ 
+
+
+
+
+
+
+
+
   // Your code starts here!
   // =========================================================================================
   // ...
-}
+
 
 // Running the code
 // ===========================================================================================
@@ -201,7 +320,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   //mousePosEvents();
   mousePosObservable();
 
-  animatedRectTimer()
+  animatedRect();
   // replace the above call with the following once you have implemented it:
   //animatedRect()
   keyboardControl();
